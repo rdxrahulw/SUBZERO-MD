93c4f619961cc636ae5c55acacb8b0907f3ce4a7
rdxrahulw <brdx19765@gmail.com>
2025-08-02 23:53:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;HBNBEIKI#6Z0aQGsjgOOKprgvwnYRttnFQHSW9KNlhIHtiqsoXe8", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "966593568767", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
